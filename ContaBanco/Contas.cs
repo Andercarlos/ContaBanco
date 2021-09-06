aeaf0fe3c8f0536b2e707a11361c67639168b938
Andercarlos <anderson.carlos.8246@gmail.com>
2021-09-06 18:26:59 -0300

    Adicionar arquivos de projeto.

---
+++ ContaBanco/Contas.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBanco
+{
+    class Contas
+    {
+        private string Nome { get; set; }
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        
+        public Contas(string nome, 
+                      TipoConta tipoConta, 
+                      double saldo, 
+                      double credito)
+        {
+            this.Nome = nome;
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+                    }
+
+     
+
+        public bool depositar(double valor)
+        {
+            this.Saldo = valor + Saldo;
+            Console.WriteLine($"Obrigado, valor de R${Saldo} depositado com sucesso!");
+            return true;
+        }
+
+        public bool sacar(double valorSaque)
+        {
+            if(this.Saldo < valorSaque)
+            {
+                Console.WriteLine($"Saldo insuficiente, Saldo atual: {this.Saldo}");
+                return false;
+            }
+            else
+            {
+                this.Saldo -= valorSaque;
+                Console.WriteLine($"Valor retirado de R${valorSaque} retirado com sucesso, Saldo atual: {this.Saldo}");
+                return true;
+            }
+           }
+        public void Transferir(double valorTransferencia, Contas contaDestino )
+        {
+            if (this.sacar(valorTransferencia))
+            {
+                contaDestino.depositar(valorTransferencia);
+            }
+        }
+        
+        public override string ToString()
+        {
+            string mensagem="";
+            mensagem += "Nome: " + this.Nome + " | ";
+            mensagem += "Saldo: " + this.Saldo + " | ";
+            mensagem += "Tipo: " + this.TipoConta + " | ";
+            mensagem += "Credito: " + this.Credito + " | ";
+            return mensagem;
+        }
+    }
+}
---
+++ ContaBanco/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContaBanco
+{
+    class Program
+    {
+        static List<Contas> Todas = new List<Contas>();
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Adicionar- 1\n Sair- 0");
+            int opcao = int.Parse(Console.ReadLine());
+            while (opcao != 0) { 
+            switch (opcao) 
+            {
+                case 1:
+                    Adicionar();
+                    foreach(Contas a in Todas)
+                    {
+                        Console.WriteLine(a);
+                    }
+                    break;
+                    
+            }
+            }
+
+
+
+            static void Adicionar()
+            {
+                Console.WriteLine("Nome?");
+                string NomeEntrada = Console.ReadLine();
+
+                Console.WriteLine("Tipo da conta 1 ou 2");
+                int TipoEntrada = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Saldo");
+                double SaldoEntrada = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Crédito Inicial?");
+                double CreditoInicial = double.Parse(Console.ReadLine());
+
+                Contas NovaConta = new Contas(tipoConta: (TipoConta)TipoEntrada,
+                                            nome: NomeEntrada,
+                                            saldo: SaldoEntrada,
+                                            credito: CreditoInicial
+                                            );
+                Todas.Add(NovaConta);
+
+
+            }
+       
+
+        }
+
+    }
+}
